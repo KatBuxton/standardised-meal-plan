5a07a9150e3924b6ea39ae985114e0bec4450839
buxton <buxton@oksystem.cz>
2023-06-13 13:47:20 +0200

    moving images to public folder

--- src/Components/Meal/Card.tsx
+++ src/Components/Meal/Card.tsx
@@ -27,7 +27,7 @@ export default function Card({season}: CardProps) {
                         <div
                             className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 h-80">
                             <img
-                                src={meal.image}
+                                src="/public/meal-images/fish.jpg"
                                 alt={meal.title}
                                 className="h-full w-full object-cover object-bottom
   lg:h-full lg:w-full"
