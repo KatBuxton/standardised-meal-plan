67f2ef8061a36f09f276302224fe62af4b00211a
buxton <buxton@oksystem.cz>
2023-11-29 11:31:32 +0100

    (Fix) - image div restyle

--- src/Components/ImageComponent.tsx
+++ src/Components/ImageComponent.tsx
@@ -65,7 +65,7 @@ export const ImageComponent: React.FC<ImageComponentProps> = ({ imageUrl }) => {
     };
 
     return (
-        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+        <div className='relative h-full w-full'>
             {isLoading && (
                 <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900">
                     <Spinner />
